475df6ac3f36df04f77c8246a3ecc1bc04bf9bee
Fabiano Gabriel Tassotti <53409044+FabianoTSS@users.noreply.github.com>
2025-07-15 14:26:20 -0300

    Update setup.sh
    Signed-off-by: Fabiano Gabriel Tassotti <53409044+FabianoTSS@users.noreply.github.com>

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "🔄 Clonando OpenWrt 18.06..."
-
-if [ ! -d "openwrt" ]; then
-  git clone https://github.com/openwrt/openwrt.git -b openwrt-18.06 openwrt
-fi
-
-cd openwrt
-
-echo "📦 Escrevendo feeds.conf com repositórios do GitHub..."
-cat > feeds.conf <<EOL
-src-git packages https://github.com/openwrt/packages.git;openwrt-18.06
-src-git luci https://github.com/openwrt/luci.git;openwrt-18.06
-src-git routing https://github.com/openwrt-routing/packages.git;openwrt-18.06
-src-git telephony https://github.com/openwrt/telephony.git;openwrt-18.06
-EOL
-
 echo "📥 Atualizando e instalando feeds..."
 ./scripts/feeds update -a
 ./scripts/feeds install -a
